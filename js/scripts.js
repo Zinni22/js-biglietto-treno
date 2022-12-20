65d24e045d125ffdf42ce6dc58ed0fa66b95ac14
Zineb Atlassi <atlassizineb@gmail.com>
2022-12-20 11:41:31 +0100

    Esercizio terminato

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 /*
 1. prompt per chiedere numero di km da percorrere
 2. prompt per chiedere l'età del passeggero
-
 3. imposto una varianta secondo la quale 1km equivale a 0.21€
 4. condizionale: 
  - se sei più piccolo di 18 hai uno sconto del 20%
- - se invece sei più grande di 64 hai uno sconto del 40%
+ - se invece sei più grande di 65 hai uno sconto del 40%
  - altrimenti il prezzo è normale
+5. Output prezzo finale
 */
 
 
@@ -20,17 +20,18 @@ let age = prompt('Quanti anni hai?');
 console.log('gli anni del passeggero sono', age);
 
 // 3. imposto prezzo biglietto normale poi gli sconti
-let ticketPrice = (chilometri * 2 + '€');
-console.log('il prezzo del biglietto è', ticketPrice);
+let ticketPrice = (chilometri * 0.21);
+console.log('il prezzo del biglietto intero è', ticketPrice);
 
-// condizioni
+
+// 4. condizioni
 if (age < 18){
-    ticketPrice = (ticketPrice - ((ticketPrice * 20) / 100));
+    ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
     alert('il tuo biglietto è ridotto del 20%');
     console.log('il prezzo del biglietto sotto ai 18 è', ticketPrice);
 }
 else if (age > 65){
-    ticketPrice = (ticketPrice - ((ticketPrice * 40) / 100));
+    ticketPrice = ticketPrice - ((ticketPrice * 40) / 100);
     alert('il tuo biglietto è ridotto del 40%');
     console.log('il prezzo del biglietto sopra ai 65 è', ticketPrice);
 }
@@ -39,4 +40,5 @@ else{
     console.log('il prezzo del biglietto intero è', ticketPrice);
 }
 
-
+// 5. output prezzo finale
+alert('Prezzo finale: €' + ticketPrice.toFixed(2))
